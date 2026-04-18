9242641b71a4e2397776f7cf75d191cc496b8721
AinisALaur <ainislaurinavicius@gmail.com>
2026-04-18 12:48:48 +0300

    Challenge 1 code

---
+++ NeetCode - two pointers/Challenge1.py
@@ -0,0 +1,14 @@
+class Solution:
+    def reverseString(self, s: list[str]) -> None:
+        i = 0
+        j = len(s) - 1
+
+        while i < j:
+            s[i], s[j] = s[j], s[i]
+            i += 1
+            j -= 1
+
+sol = Solution()
+str = list("asdfghjklqwertyuiopzxcvbnm")
+sol.reverseString(str)
+print("".join(str))
